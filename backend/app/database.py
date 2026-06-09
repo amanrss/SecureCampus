86d92247099e0583aa13de85e763ebab3891d14e
Aman Singh <141247273+amanrss@users.noreply.github.com>
2026-06-09 23:51:18 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
 from .config import MONGO_URI, DB_NAME
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(
+    MONGO_URI,
+    serverSelectionTimeoutMS=10000
+)
 db = client[DB_NAME]
 faces_col = db["faces"]
 logs_col = db["logs"]
